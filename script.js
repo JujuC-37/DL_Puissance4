d75aa5650ecdf1ea62d9896054e70313ac27d022
Juliette-C <juliette.coustard@gmail.com>
2017-12-07 10:37:29 +0100

    JS: added stacking chips

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const $firstRow = $('.row:first-of-type');
-let nbTours = 0;
+const $message = $('.message');
 
+const colorPlayer1 = 'blue';
+const colorPlayer2 = 'red';
+let currentPlayer = 'Joueur 1';
+
+updateMessage();
+
+// ----------------------- events ------------------------
 $firstRow.on('click', '.cell', function (event) {
-    nbTours += 1;
-    const actualColor = (nbTours%2 == 1 ? 'blue' : 'red');
-
     const $cell = $(event.target);
   
     const i = $cell.index() + 1;
-  
-    $(`.row:last-of-type > .cell:nth-of-type(${i})`)
-        .addClass(actualColor);
+
+    if (currentPlayer === 'Joueur 1') {
+        addColorToCell(i,colorPlayer1);
+        currentPlayer = 'Joueur 2';
+    }
+    else {
+        addColorToCell(i,colorPlayer2);
+        currentPlayer = 'Joueur 1';
+    }
+    updateMessage();
 });
+
+// ---------------------- functions ----------------------
+function updateMessage() {
+  $message.text(`Joueur actuel : ${currentPlayer}`)
+}
+
+function addColorToCell(numColumn, color) {
+    let emptyCase = false;
+    let i = 1;
+
+    while($(`.row:nth-last-of-type(${i}) > .cell:nth-of-type(${numColumn})`).is(`.${colorPlayer1}, .${colorPlayer2}`)) {
+        i++;
+    }
+
+    $(`.row:nth-last-of-type(${i}) > .cell:nth-of-type(${numColumn})`).addClass(color);
+
+}
